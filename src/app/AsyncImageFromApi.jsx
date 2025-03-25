6de6c44aeda6c5d17540017d8f8cd78813f46220
Andrew Murray <andyated@gmail.com>
2025-03-25 13:31:34 +0000

    Looks like I've been forgetting to commit AsyncImageFromApi.jsx, oopseh.

---
+++ src/app/AsyncImageFromApi.jsx
@@ -0,0 +1,60 @@
+'use client'
+
+import React from 'react'
+import { readFile } from '@tauri-apps/plugin-fs';
+import Skeleton from '@mui/material/Skeleton';
+// polyfill for toBase64
+import "core-js/modules/esnext.uint8-array.to-base64.js";
+import { invoke } from '@tauri-apps/api/core';
+
+export default function AsyncImageFromApi({image, imageStyle, width, height})
+{
+  const [imgState, setImageState] = React.useState(null);
+  const [orientation, setOrientation] = React.useState(0);
+  React.useEffect(
+    () => {
+      let mounted = true;
+      const awaitable = async () => {
+        const contents = await invoke(
+          "get_image_for_id",
+          {
+            imageId: image.imageid.toString(),
+            mode: "lo"
+          }
+        );
+        const base64string = new Uint8Array(contents).toBase64();
+        // todo: it's a better pattern to abort the request according to react sources
+        // though ... this seems to catch more cases we care about
+        if (mounted)
+        {
+          setImageState(`data:image/jpg;base64,${base64string}`);
+        }
+      };
+      awaitable();
+      return ()=>{ mounted=false; };
+    },
+    [image]
+  );
+
+  const oClassMap = {
+    0: undefined,
+    90: "rotate90",
+    180: "rotate180",
+    270: "rotate270"
+  };
+
+  const oClass = oClassMap[orientation];
+
+  return <React.Fragment>
+    {imgState === null && <Skeleton variant="rectangular" width={width} height={height} /> }
+    {imgState != null && <img
+      src={imgState}
+      class={oClass}
+      style={Object.assign(
+        {},
+        imageStyle,
+        {width, height}
+      )}
+    />}
+  </React.Fragment>
+}
